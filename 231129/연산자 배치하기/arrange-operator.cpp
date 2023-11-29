cce9220ecc4efa884bc5902419a34a2408e45306
simonseo22 <40517127+simonseo22@users.noreply.github.com>
2023-11-29 20:28:01 +0900

    Title: 연산자 배치하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231129/연산자 배치하기/arrange-operator.cpp
+++ 231129/연산자 배치하기/arrange-operator.cpp
@@ -6,7 +6,7 @@
 using namespace std;
 
 int arr[NUM_MAX];
-int op[NUM_MAX-1]; // 1 : 덧셈, 0 : 뺄셈, -1 : 곱셈
+int op[NUM_MAX-1]; // 1 : 덧셈, 0 : 곱셈, -1 : 뺄셈
 int n;
 int add, sub, mul;
 int MAX = numeric_limits<int>::min();
@@ -15,18 +15,36 @@ int MIN = numeric_limits<int>::max();
 
 void caculate(){
     int temp = arr[0];
+    if (DEBUG)
+    {
+        cout << arr[0] << " ";
+    }
+    
     for (int i = 0; i < n-1; i++)
     {
         switch (op[i])
         {
-        case 0:
-            temp += arr[i+1];
-            break;
         case 1:
-            temp -= arr[i+1];
+            temp += arr[i+1];
+            if (DEBUG)
+            {
+                cout << "+" << " ";
+            }
+            
+            break;
+        case 0:
+            temp *= arr[i+1];
+            if (DEBUG)
+            {
+                cout << "*" << " ";
+            }
             break;
         case -1:
-            temp *= arr[i+1];
+            temp -= arr[i+1];
+            if (DEBUG)
+            {
+                cout << "-" << " ";
+            }
             break;
 
         default:
@@ -37,12 +55,30 @@ void caculate(){
             
             break;
         }
+        if (DEBUG)
+        {
+            cout << arr[i+1] << " ";
+        }
+    }
+    if (DEBUG)
+    {
+        cout << endl;
     }
     MAX = temp > MAX ? temp : MAX;
-    MIN = temp < MIN ? temp : MIN;    
+    MIN = temp < MIN ? temp : MIN;
+    if (DEBUG)
+    {
+        printf("temp : %d, MAX : %d, MIN : %d\n", temp, MAX, MIN);
+    }
 }
 
 void simulate(int index, int add_num, int sub_num, int mul_num){
+    if (DEBUG)
+    {
+        printf("index : %d, add_num : %d, sub_num : %d, mul_num : %d\n",
+        index, add_num,sub_num,mul_num);
+    }
+    
     if (add_num > add 
     || sub_num > sub
     || mul_num > mul
@@ -56,15 +92,17 @@ void simulate(int index, int add_num, int sub_num, int mul_num){
     && mul_num == mul)
     {
         caculate();
+        return;
     }
-    op[index] = 1;
-    simulate(index+1,add_num+1, sub_num, mul_num);
-    
-    op[index] = 0;
-    simulate(index+1,add_num, sub_num+1, mul_num);
 
+    op[index] = 1;
+    simulate(index+1, add_num+1, sub_num, mul_num);
+    
     op[index] = -1;
-    simulate(index+1,add_num, sub_num, mul_num+1);
+    simulate(index+1, add_num, sub_num+1, mul_num);
+
+    op[index] = 0;
+    simulate(index+1, add_num, sub_num, mul_num+1);
 }
 
 int main() {
@@ -74,11 +112,10 @@ int main() {
         cin >> arr[i];
     }
 
-    int add, sub, mul;
     cin >> add >> sub >> mul;
 
     simulate(0,0,0,0);
     
-    cout << MIN << MAX << endl;
+    cout << MIN  << " " << MAX << endl;
     return 0;
 }
