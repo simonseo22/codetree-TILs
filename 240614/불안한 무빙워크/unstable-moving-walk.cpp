1d41256131c90137fa82e1355e23a7ed3dce7265
simonseo22 <40517127+simonseo22@users.noreply.github.com>
2024-06-14 20:37:27 +0900

    Title: 불안한 무빙워크, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240614/불안한 무빙워크/unstable-moving-walk.cpp
+++ 240614/불안한 무빙워크/unstable-moving-walk.cpp
@@ -11,7 +11,7 @@
 #include <vector>
 #include <queue>
 
-#define DEBUG 0
+#define DEBUG 1
 #define N_MAX 100 
 
 using namespace std;
@@ -43,20 +43,20 @@ bool IsEnd() {
 	{
 		return false;
 	}
-	
+
 }
 
 void Rotate() {
 	// 사람이동 추가
 	Marr[n - 1] = 0;
-	for (int i = n-1; i >= 1; i--)
+	for (int i = n - 1; i >= 1; i--)
 	{
 		Marr[i] = Marr[i - 1];
 	}
 	Marr[0] = 0;
 
 
-	int uLast = uarr[n-1];
+	int uLast = uarr[n - 1];
 	for (int i = n - 1; i >= 1; i--)
 	{
 		uarr[i] = uarr[i - 1];
@@ -68,7 +68,7 @@ void Rotate() {
 		darr[i] = darr[i - 1];
 	}
 	uarr[0] = dLast;
-	darr[0] = uLast;	
+	darr[0] = uLast;
 #if DEBUG
 	printf("Rotate :%d\n", tryCount);
 	printf("Marr\n");
@@ -97,29 +97,26 @@ void Rotate() {
 }
 
 void ManMove() {
-	for (int i = n-1; i >= 0; i--) // 앞쪽에 있는 사람이 먼저온 사람
+	for (int i = n - 1; i >= 0; i--) // 앞쪽에 있는 사람이 먼저온 사람
 	{
-		if (Marr[i] == 1) 
+		if (Marr[i] == 1)
 		{
-			for (int j = i; j < n; j++) // 해당사람이 걸어갈
-			{				
-				if (j == n-1)
-				{
-					Marr[j] = 0;
-				}
-				else if (j < n-1 && uarr[j+1] > 0 && Marr[j+1] == 0) // 끝이 아니고, 무빙워크 내구도가 있고, 앞에 사람없고
-				{
-					// 사람이동
-					Marr[j] = 0;
-					Marr[j + 1] = 1;
-					
-					// 이동위치 내구도 -1
-					uarr[j+1] -= 1;
-				}
-				else if(uarr[j + 1] == 0 || Marr[j + 1] == 1) // 사람이 있거나 내구도가 0인경우 이동 멈춤(찾지못함) -> 모든 경우를 정리해야한다. 안그러면 구현중 까먹음
-				{
-					break;
-				}
+			if (i == n - 1)
+			{
+				Marr[i] = 0;
+			}
+			else if (i < n - 1 && uarr[i + 1] > 0 && Marr[i + 1] == 0) // 끝이 아니고, 무빙워크 내구도가 있고, 앞에 사람없고
+			{
+				// 사람이동
+				Marr[i] = 0;
+				Marr[i + 1] = 1;
+
+				// 이동위치 내구도 -1
+				uarr[i + 1] -= 1;
+			}
+			else if (uarr[i + 1] == 0 || Marr[i + 1] == 1) // 사람이 있거나 내구도가 0인경우 이동 멈춤(찾지못함) -> 모든 경우를 정리해야한다. 안그러면 구현중 까먹음
+			{
+				//break;
 			}
 		}
 	}
@@ -192,7 +189,7 @@ int main() {
 	{
 		tryCount++;
 		Rotate();
-		ManMove();		
+		ManMove();
 		if (uarr[0] > 0)
 		{
 			uarr[0] -= 1;
@@ -215,7 +212,7 @@ int main() {
 		cout << endl;
 
 		printf("darr\n");
-		for (int i = n-1; i >= 0; i--)
+		for (int i = n - 1; i >= 0; i--)
 		{
 			cout << darr[i] << " ";
 		}
